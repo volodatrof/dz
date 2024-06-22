4ece73ef3217de963d54565abc0c2156fe213c03
voloda <voloda@DESKTOP-H68HFR6>
2024-06-22 17:51:16 +0300

    Визначення значення функції в точці

---
+++ Determining the value of a function at a point/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Determining_the_value_of_a_function_at_a_point
+{
+    using System;
+
+    class LagrangeInterpolation
+    {
+        public static double Lagrange(double[] x, double[] y, double xToPredict)
+        {
+            int n = x.Length;
+            double result = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double term = y[i];
+                for (int j = 0; j < n; j++)
+                {
+                    if (j != i)
+                    {
+                        term *= (xToPredict - x[j]) / (x[i] - x[j]);
+                    }
+                }
+                result += term;
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            double[] x = { 2.10, 2.67, 3.01, 3.82 };
+            double[] y = { 122.23, 123.45, 120.02, 119.65 };
+
+            Console.WriteLine("Enter the value of x to predict:");
+                double xToPredict = Convert.ToDouble(Console.ReadLine());
+
+            double predictedValue = Lagrange(x, y, xToPredict);
+            Console.WriteLine($"The predicted value at x = {xToPredict} is y = {predictedValue:F2}");
+        }
+    }
+
+}
